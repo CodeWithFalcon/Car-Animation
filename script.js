2d5a06b71f2cc92f2cfdd2695336b3821de15242
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-28 09:58:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3'); 
-audio.loop=true;
-audio.play();
+// Create audio
+const audio = new Audio("assets/Sound/sound.mp3");
+audio.loop = true;
+audio.volume = 0.5;
+
+// Browsers block autoplay → fix with user interaction
+document.addEventListener("click", () => {
+    audio.play().catch(() => {
+        console.log("Click again to enable sound");
+    });
+}, { once: true });
+
+console.log("Car animation loaded successfully 🚗");
